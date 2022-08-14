aae8fefb2a9a0b656a7b897cbf82f8fe51cbd01d
ali_jarkani <alijarkani@gmail.com>
2022-08-14 20:42:18 +0430

    Prevent infinite loop

--- bots/crazy.py
+++ bots/crazy.py
@@ -10,7 +10,7 @@ class CrazyAI(Player):
         super().__init__(title, color)
 
     def play(self, board: Board):
-        while True:
+        while board.has_empty_cell:
             row = random.randint(0, board.size - 1)
             col = random.randint(0, board.size - 1)
 
